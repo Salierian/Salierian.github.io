cca078b57ff0b97c853160622eb4475a8074b602
Salierian <163623701+Salierian@users.noreply.github.com>
2024-04-09 20:50:43 -0600

    gato
    gatoboton

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+let indexGato = Math.floor(Math.random() * 10);
+let gatostring = "assets/gatosm/" + indexGato + ".jpeg";
+console.log(gatostring);
+
+document.getElementById("gatorango").setAttribute("src", gatostring);
+
+
